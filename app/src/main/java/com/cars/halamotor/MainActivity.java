16836a6d1ca30f904c0e71b32fb378a2e0ba0c92
mohammadObeidat <mohammadkhadeer1992@gmail.com>
2020-01-29 03:58:41 +0400

    Connect on firebase database and set red and write method in main screen just to test

--- app/src/main/java/com/cars/halamotor/MainActivity.java
+++ app/src/main/java/com/cars/halamotor/MainActivity.java
@@ -3,9 +3,16 @@ package com.cars.halamotor;
 import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.BottomBarTab;
 import com.roughike.bottombar.OnTabReselectListener;
@@ -14,6 +21,8 @@ import com.roughike.bottombar.OnTabSelectListener;
 public class MainActivity extends AppCompatActivity {
     private TextView messageView;
     BottomBar bottomBar;
+    private DatabaseReference mDatabase;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +31,35 @@ public class MainActivity extends AppCompatActivity {
 
         inti();
         BottomBarMenu();
+        writeOnDataBase();
+        readFromDataBase();
 
+
+    }
+
+    private void readFromDataBase() {
+        DatabaseReference mRef = FirebaseDatabase.getInstance().getReference();
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        mRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                //ItemDetails user = dataSnapshot.getValue(ItemDetails.class);
+                Log.i("TAG",dataSnapshot.toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {}
+
+        });
+    }
+
+    private void writeOnDataBase() {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
     }
 
     private void inti() {
