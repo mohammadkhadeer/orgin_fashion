167bae57b978ef006314453e1ddc21323e440cda
mohammadObeidat <mohammadkhadeer1992@gmail.com>
2020-01-28 21:51:19 +0400

    Add bottom bar menu on main screen also add "TabMessage" class contain bottom bar menu id must to change name in adv also add bottom bar icons

--- app/src/main/java/com/cars/halamotor/MainActivity.java
+++ app/src/main/java/com/cars/halamotor/MainActivity.java
@@ -1,13 +1,53 @@
 package com.cars.halamotor;
 
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.BottomBarTab;
+import com.roughike.bottombar.OnTabReselectListener;
+import com.roughike.bottombar.OnTabSelectListener;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView messageView;
+    BottomBar bottomBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inti();
+        BottomBarMenu();
+
+    }
+
+    private void inti() {
+        messageView = (TextView) findViewById(R.id.messageView);
+        bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+    }
+
+    private void BottomBarMenu() {
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(@IdRes int tabId) {
+                messageView.setText(TabMessage.get(tabId, false));
+            }
+        });
+
+        bottomBar.setOnTabReselectListener(new OnTabReselectListener() {
+            @Override
+            public void onTabReSelected(@IdRes int tabId) {
+                Toast.makeText(getApplicationContext(), TabMessage.get(tabId, true), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        BottomBarTab messages = bottomBar.getTabWithId(R.id.tab_messages);
+        BottomBarTab notifications = bottomBar.getTabWithId(R.id.tab_notifications);
+        messages.setBadgeCount(13);
+        notifications.setBadgeCount(1);
     }
 }
